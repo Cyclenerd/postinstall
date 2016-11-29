35695c5f0d5a9da291d150c913c12a5fd5ef13d8
Nils <git@velohero.com>
2016-11-29 20:47:16 +0100

    DEBIAN support

---
+++ base/DEBIAN/README.md
@@ -0,0 +1,10 @@
+# Debian
+
+Debian is a Unix-like computer operating system. Ubuntu is a Debian-based Linux operating system.
+
+## Using APT for Binary Package Management
+
+`apt-get` is a package management tool for Debian and other Linux distributions.
+
+It is already installed in the standard. You do not have to do anything.
+
---
+++ base/DEBIAN/workstation/before.sh
@@ -0,0 +1,30 @@
+#
+# before.sh for DEBIAN and TYPE=workstation
+#
+
+# Add the Spotify repository signing key to be able to verify downloaded packages
+echo_step "  Installing Spotify repository signing key"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 >>"$INSTALL_LOG" 2>&1
+if [ "$?" -ne 0 ]; then
+	echo_warning "Failed to install signing key, will attempt to continue"
+else
+	echo_success
+fi
+
+# Add the Spotify repository
+echo_step "  Installing Spotify repository"
+deb http://repository.spotify.com stable non-free | tee "/etc/apt/sources.list.d/spotify.list" >>"$INSTALL_LOG" 2>&1
+if [ "$?" -ne 0 ]; then
+	echo_warning "Failed to install repositories, will attempt to continue"
+else
+	echo_success
+fi
+
+# Update list of available packages
+echo_step "  Update list of available packages"
+apt-get update >>"$INSTALL_LOG" 2>&1
+if [ "$?" -ne 0 ]; then
+	echo_warning "Failed to install repositories, will attempt to continue"
+else
+	echo_success
+fi
