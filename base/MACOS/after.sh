654cf8e8f8a29dd39ffe0dfa649522b73a23869f
Nils <git@velohero.com>
2016-12-04 17:44:30 +0100

    macos system defaults

--- base/MACOS/after.sh
+++ base/MACOS/after.sh
@@ -2,6 +2,41 @@
 # after.sh for MACOS and all TYPEs
 #
 
+
+#
+# Set system defaults for macOS
+#    https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#
+
+echo_step "  Set system defaults for macOS"
+
+# Disable the sound effects on boot
+nvram SystemAudioVolume=" "
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Restart automatically if the computer freezes
+systemsetup -setrestartfreeze on
+
+# Never go into computer sleep mode
+systemsetup -setcomputersleep Off > /dev/null
+
+# Set the timezone; see `sudo systemsetup -listtimezones` for other values
+systemsetup -settimezone "Europe/Berlin" > /dev/null
+
+# Show the /Volumes folder
+chflags nohidden /Volumes
+
+# Kill affected applications
+for app in "Dock" "Finder"; do
+	killall "${app}" &> /dev/null
+done
+
+echo_success
+
+
 #
 # Trigger a Notification Center notification from an AppleScript
 #
