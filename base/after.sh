b1bd790dcb08452638efbc29db24e7f64f1eb392
Nils <git@velohero.com>
2016-12-04 17:56:38 +0100

    set bashrc for root

---
+++ base/after.sh
@@ -0,0 +1,28 @@
+#
+# after.sh for all OPERATING_SYSTEMs and TYPEs
+#
+
+#
+# bashrc for root
+#
+
+# .profile or .bashrc
+if [ -d "/root" ]; then
+	echo_step "  Set .bashrc for root"
+	if [ -f "/root/.bashrc" ]; then
+		cp "/root/.bashrc" "/root/.bashrc.$OPERATING_SYSTEM"
+	fi
+	cat >> "/root/.bashrc" << EOF
+
+# Set RED prompt
+PS1='\[\033[01;31m\]\u@\h \[\033[01;34m\]\W \$ \[\033[00m\]'; export PS1
+
+# Define nano as our default EDITOR
+export EDITOR='nano'
+
+export LC_CTYPE='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
+
+EOF
+	echo_success
+fi
