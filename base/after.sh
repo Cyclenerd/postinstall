f84acebba3dab48f68be38233371d0fe235b39a4
Nils <git@velohero.com>
2017-08-24 21:54:13 +0200

    check /root

--- base/after.sh
+++ base/after.sh
@@ -56,13 +56,13 @@ fi
 # bashrc for root
 #
 
-# .profile or .bashrc
 if [ -d "/root" ]; then
 	echo_step "  Set .bashrc for root"
-	if [ -f "/root/.bashrc" ]; then
-		cp "/root/.bashrc" "/root/.bashrc.$OPERATING_SYSTEM"
-	fi
-	cat >> "/root/.bashrc" << EOF
+	if [ -w "/root" ]; then
+		if [ -f "/root/.bashrc" ]; then
+			cp "/root/.bashrc" "/root/.bashrc.$OPERATING_SYSTEM"
+		fi
+		cat >> "/root/.bashrc" << EOF
 
 # Set RED prompt
 PS1='\[\033[01;31m\]\u@\h \[\033[01;34m\]\W # \[\033[00m\]'; export PS1
@@ -74,5 +74,8 @@ export LC_CTYPE='en_US.UTF-8'
 export LC_ALL='en_US.UTF-8'
 
 EOF
-	echo_success
+		echo_success
+	else
+		echo_warning "Can not write to /root, will attempt to continue"
+	fi
 fi
