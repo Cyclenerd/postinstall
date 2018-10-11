058f24fb6bc9a0bce766694ede29618266c9853e
Nils <git@velohero.com>
2018-10-11 19:55:19 +0200

    ubuntu motd

--- base/DEBIAN/after.sh
+++ base/DEBIAN/after.sh
@@ -11,3 +11,13 @@
 echo_step "  Set the swappiness (vm.swappiness=10)"
 echo "vm.swappiness=10" >> "/etc/sysctl.conf"
 echo_success
+
+#
+# Edit motd
+#
+
+echo_step "  Edit motd"
+chmod -x "/etc/update-motd.d/10-help-text" >>"$INSTALL_LOG" 2>&1
+chmod -x "/etc/update-motd.d/50-motd-newst" >>"$INSTALL_LOG" 2>&1
+chmod -x "/etc/update-motd.d/80-livepatch" >>"$INSTALL_LOG" 2>&1
+echo_success
